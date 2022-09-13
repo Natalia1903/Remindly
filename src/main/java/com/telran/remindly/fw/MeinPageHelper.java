b819ee690875c57788211e8ca1bdd13c70bcbe44
Natalia1903 <98782051+Natalia1903@users.noreply.github.com>
2022-09-13 08:14:24 +0200

    Initial commit

---
+++ src/main/java/com/telran/remindly/fw/MeinPageHelper.java
@@ -0,0 +1,12 @@
+package com.telran.remindly.fw;
+
+import io.appium.java_client.AppiumDriver;
+
+public class MeinPageHelper extends HelperBase{
+
+    public MeinPageHelper(AppiumDriver driver){
+        super(driver);
+
+    }
+
+}
---
+++ src/test/java/com/telran/remindly/tests/MainPageTestes.java
@@ -0,0 +1,10 @@
+package com.telran.remindly.tests;
+
+import org.testng.annotations.Test;
+
+public class MainPageTestes extends  TestsBase{
+    @Test
+    public void launchAppTest(){
+        System.out.println("App opened!");
+    }
+}
